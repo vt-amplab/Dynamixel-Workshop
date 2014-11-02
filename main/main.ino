c00d9bfc0c0dbbf1920c5d2f477f9c256e2dc894
Conor <conorpp@vt.edu>
2014-11-02 17:03:17 +0000

    added code

---
+++ main/main.ino
@@ -0,0 +1,57 @@
+#include "dynamixel.h"
+
+void setup()
+{
+  Serial.begin(MEGABIT);
+}
+
+
+// Variables for demo
+int led = 0;
+int pos = 0;
+
+// Variables for state machine
+typedef enum{
+  MovingUp = 0,
+  MovingDown
+} State;
+
+void loop()
+{
+  State state = MovingUp;
+  while(1){
+    delay(5);
+    switch(state)
+    {
+      case MovingUp:
+        // ID = 1, LED address = 25, blue = 3
+        sendPacket(1, 25, 4);
+        pos++;
+        if (pos > 1022)
+        {
+          state = MovingDown;
+        }
+      break;
+      
+      case MovingDown:
+        // ID = 1, LED address = 25, yellow = 3
+        sendPacket(1, 25, 3);
+        pos--;
+        if (pos < 2)
+        {
+          state = MovingUp;
+        }
+      break;
+      default:
+        // ID = 1, LED address = 25, red = 1 (ERROR!)
+        sendPacket(1, 25, 1);
+      break;
+    }
+    delay(1);
+    // ID = 1, goal position address = 30, pos = pos
+    sendPacket(1, 30, pos);
+  }
+  
+  
+}
+
