ca4570041a86ca37f2c29c2d6cf1114ecdc98eac
conorpp <conorpp@vt.edu>
2014-11-09 14:42:12 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,8 @@
 #include "dynamixel.h"
 
+#define NEWID 2
+using namespace std;
+
 void setup()
 {
   Serial.begin(MEGABIT);
@@ -9,6 +12,7 @@ void setup()
 // Variables for demo
 int led = 0;
 int pos = 0;
+int ID = 1;
 
 // Variables for state machine
 typedef enum{
@@ -19,13 +23,19 @@ typedef enum{
 void loop()
 {
   State state = MovingUp;
+  
+ #ifdef NEWID
+   // old ID = 1, ID address = 3, ID = NEWID
+   // Note: you must specify the old ID to set the new ID..
+   sendPacketByte(1, 3, NEWID);
+ #endif 
   while(1){
     delay(5);
     switch(state)
     {
       case MovingUp:
-        // ID = 1, LED address = 25, blue = 3
-        sendPacket(1, 25, 4);
+        // ID = 1, LED address = 25, blue = 4
+        sendPacket(ID, 25, 4);
         pos++;
         if (pos > 1022)
         {
@@ -35,7 +45,7 @@ void loop()
       
       case MovingDown:
         // ID = 1, LED address = 25, yellow = 3
-        sendPacket(1, 25, 3);
+        sendPacket(ID, 25, 3);
         pos--;
         if (pos < 2)
         {
@@ -44,14 +54,13 @@ void loop()
       break;
       default:
         // ID = 1, LED address = 25, red = 1 (ERROR!)
-        sendPacket(1, 25, 1);
+        sendPacket(ID, 25, 1);
       break;
     }
     delay(1);
     // ID = 1, goal position address = 30, pos = pos
-    sendPacket(1, 30, pos);
+    sendPacket(ID, 30, pos);
   }
   
   
 }
-
